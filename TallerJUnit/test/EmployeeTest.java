e73c71d909a4c8a125f065f6efee3ec9d9a97d09
caloja1014 <caloja@espol.edu.ec>
2020-08-26 12:06:27 -0500

    Creacon del test para verificar el metodo no retorne null

--- TallerJUnit/test/EmployeeTest.java
+++ TallerJUnit/test/EmployeeTest.java
@@ -16,38 +16,59 @@ import taller.EmployeeType;
  * @author CORE i7 ULTIMATE
  */
 public class EmployeeTest {
-    
+
     private Employee e1;
     private Employee e2;
     private Employee e3;
     private Employee e4;
     private Employee e5;
     private Employee e6;
-    
-    public EmployeeTest(){
-        e1 = new Employee(100f,"USD",1f,EmployeeType.Manager); //Carlos
-        e2 = new Employee(100f,"USD",1f,EmployeeType.Supervisor); //Carlos
-        e3 = new Employee(100f,"USD",1f,EmployeeType.Worker); // Valeria
-        e4 = new Employee(100f,"EUR",1f,EmployeeType.Manager); // Jaime
-        e5 = new Employee(100f,"EUR",1f,EmployeeType.Supervisor); //Eddo
-        e6 = new Employee(100f,"EUR",1f,EmployeeType.Worker); //Alex
+
+    public EmployeeTest() {
+        e1 = new Employee(100f, "USD", 1f, EmployeeType.Manager); //Carlos
+        e2 = new Employee(100f, "USD", 1f, EmployeeType.Supervisor); //Carlos
+        e3 = new Employee(100f, "USD", 1f, EmployeeType.Worker); // Valeria
+        e4 = new Employee(100f, "EUR", 1f, EmployeeType.Manager); // Jaime
+        e5 = new Employee(100f, "EUR", 1f, EmployeeType.Supervisor); //Eddo
+        e6 = new Employee(100f, "EUR", 1f, EmployeeType.Worker); //Alex
     }
+
+    /**
+     * Test que prueba que el metodo cs calcule de forma correcta el decimo de
+     * un empleado Manager que tenga como salario 100 USD y 1 de bonusPercentaje
+     *
+     *
+     */
     @Test
-    public void csTest1(){
+    public void csTest1() {
         int LocalDate = new Date().toInstant().atZone(ZoneId.systemDefault()).toLocalDate().getMonthValue();
-        Float value=100.0f+0.7f;
-        Float operacion=LocalDate%2==0?value:value+(386f/6f);
-        Assert.assertEquals((Float)e1.cs(),operacion);
-        
+        Float value = 100.0f + 0.7f;
+        Float operacion = LocalDate % 2 == 0 ? value : value + (386f / 6f);
+        Assert.assertEquals((Float) e1.cs(), operacion);
+
     }
+
+    /**
+     * Test que prueba que el metodo cs calcule de forma correcta el decimo de
+     * un empleado Supervisor que tenga como salario 100 USD y 1 de
+     * bonusPercentaje
+     *
+     */
     @Test
-    public void csTest2(){
+    public void csTest2() {
         int LocalDate = new Date().toInstant().atZone(ZoneId.systemDefault()).toLocalDate().getMonthValue();
-        Float value=100.0f+0.35F;
-        Float operacion=LocalDate%2==0?value:value+(386f/6f);
-        Assert.assertEquals((Float)e2.cs(),operacion);
-        
+        Float value = 100.0f + 0.35F;
+        Float operacion = LocalDate % 2 == 0 ? value : value + (386f / 6f);
+        Assert.assertEquals((Float) e2.cs(), operacion);
+
     }
-  
-    
+    /**
+     * Test que prueba que el metodo no retorne un valor nulo
+     *
+     */
+    @Test
+    public void csTest3(){
+        Assert.assertNotNull(e1);
+    }
+
 }
